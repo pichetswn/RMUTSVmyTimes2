e4d75e3d5d44bee1d9aba48656a63b3a1ed0a191
pichetswn <pichetswn@gmail.com>
2018-01-25 12:46:55 +0700

    Initial commit

---
+++ app/src/main/java/pichetswn/rmutsv/ac/th/rmutsvmytimes2/MainActivity.java
@@ -0,0 +1,28 @@
+package pichetswn.rmutsv.ac.th.rmutsvmytimes2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pichetswn.rmutsv.ac.th.rmutsvmytimes2.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment of Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+
+
+    } //Main Method
+
+
+} // Main Class
