17f530d6e33ad0b06743a2f11136fe544b7de7b0
pichetswn <pichetswn@gmail.com>
2018-01-26 09:51:06 +0700

    Add GraphFragment Success

--- app/src/main/java/pichetswn/rmutsv/ac/th/rmutsvmytimes2/GraphActivity.java
+++ app/src/main/java/pichetswn/rmutsv/ac/th/rmutsvmytimes2/GraphActivity.java
@@ -3,11 +3,23 @@ package pichetswn.rmutsv.ac.th.rmutsvmytimes2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import pichetswn.rmutsv.ac.th.rmutsvmytimes2.fragment.GraphFragment;
+
 public class GraphActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentGraphFragment, new GraphFragment())
+                    .commit();
+        }
+
+    }  // Main Method
+
+}  // Main Class
